2b13c0561357b778ea51f58c8c66227997074ed9
Sanjampreet Singh <sanjam531@gmail.com>
2025-11-15 01:37:46 +0530

    feat: Add LightRays component and integrate into layout; update home page content and styles; add utility functions and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 const Home = () => {
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <section>
+      <h1 className="text-center">The Hub for every Dev<br/>Event you can&apos;t miss!</h1>
+      <p className="text-center mt-5">Hackathon, Conferences, Meetups, and more! All in one place!</p>
+    </section>
   );
 };
 

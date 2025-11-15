be96c70f176479b8c86ade58fb1abcc7533ae91b
Sanjampreet Singh <sanjam531@gmail.com>
2025-11-15 23:26:57 +0530

    feat: Add ExploreBtn component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const Home = () => {
   return (
     <section>
-      <h1 className="text-center">The Hub for every Dev<br/>Event you can&apos;t miss!</h1>
-      <p className="text-center mt-5">Hackathon, Conferences, Meetups, and more! All in one place!</p>
+      <h1 className="text-center">
+        The Hub for every Dev
+        <br />
+        Event you can&apos;t miss!
+      </h1>
+      <p className="text-center mt-5">
+        Hackathon, Conferences, Meetups, and more! All in one place!
+      </p>
+      <ExploreBtn />
     </section>
   );
 };
 
 export default Home;
+
+

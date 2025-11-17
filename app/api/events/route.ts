9b11e423f0c2ae29147c63d30c11b6a09eededb1
Sanjampreet Singh <sanjam531@gmail.com>
2025-11-17 12:47:51 +0530

    feat: Implement POST API for event creation and add environment configuration

---
+++ app/api/events/route.ts
@@ -0,0 +1,21 @@
+import { Event } from "@/database";
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectDB();
+        const formData = await req.formData();
+        let event;
+        try {
+            event = Object.fromEntries(formData.entries())
+        } catch (e) {
+            return NextResponse.json({ message: 'Invalid form data' }, { status: 400 });
+        }
+        const createdEvent = await Event.create(event);
+        return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 }); 
+    } catch (error) {
+        console.error("Error handling POST /events:", error);
+        return NextResponse.json({ message: 'Event creation failed', error: error instanceof Error ? error.message : 'Unknown' }, { status: 500 });
+    }
+}

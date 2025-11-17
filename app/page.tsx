3d830db251e016ad9430ee0ce98ae6fa4f01625c
Sanjampreet Singh <sanjam531@gmail.com>
2025-11-18 01:14:50 +0530

    feat: Implement GET API for fetching event by slug and enhance error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import { events } from "@/lib/constants";
+import { IEvent } from "@/database";
 
-const Home = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const Home = async () => {
+  const eventsResponse = await fetch(`${BASE_URL}/api/events`, {
+    cache: "no-store",
+  });
+  const eventsData = await eventsResponse.json();
+  const events = eventsData.events;
+
   return (
     <section>
       <h1 className="text-center">
@@ -17,11 +24,13 @@ const Home = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events">
-          {events.map((event) => (
-            <li key={event.title}>
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
